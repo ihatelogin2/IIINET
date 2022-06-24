ac53a71f61b34db8f7788e010f7e5884b6922ff2
Kacper Bury <ihatelogin2@gmail.com>
2022-06-24 21:08:46 +0200

    add new files

---
+++ III_serwisy_net/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace III_serwisy_net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var klient = new DHL.DHL24WebapiPortClient();
+            String wynik = klient.getVersion();
+            Console.WriteLine(wynik);
+            Console.ReadLine();
+        }
+    }
+}
